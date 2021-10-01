ca1d4295798b6bf2c4ab693d21c7f847d59d1ac9
Csilla Kasnya <csilla.kasnya@stud.uni-corvinus.hu>
2021-10-01 22:56:37 +0200

    7

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace UserMaintenance
 {
-    public partial class Form1 : Form
+    BindingList<User> users = new BindingList<User>();
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+        label1.Text = Resource.FullName;
+        button1.Text = Resource.Add;
+
+        listBox1.DataSource = users;
+        listBox1.ValueMember = "ID";
+        listBox1.DisplayMember = "FullName";
+
+        var u = new User()
         {
-            InitializeComponent();
-        }
+            FullName = textBox1.Text,
+
+        };
+        users.Add(u);
     }
 }
