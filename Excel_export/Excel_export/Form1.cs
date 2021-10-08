384828f61ff27f566750bf6da0aa91af1b60c91e
Csilla Kasnya <csilla.kasnya@stud.uni-corvinus.hu>
2021-10-08 13:34:01 +0200

    6

--- Excel_export/Excel_export/Form1.cs
+++ Excel_export/Excel_export/Form1.cs
@@ -16,16 +16,50 @@ namespace Excel_export
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
             
         }
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value); //üres munkafüzet
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error:{0}\nLine: {1}", ex.Message,ex.Source);
+                MessageBox.Show(hiba, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null; //sorrend fontos
+                xlApp = null;
+                throw;
+            }
+            
+        }
+        private void CreateTable()
+        {
+
         }
     }
 }
