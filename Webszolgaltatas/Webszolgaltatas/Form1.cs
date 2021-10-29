605b8bdfef0fc36ba419c5dc36c53547cd5d05ed
Csilla Kasnya <csilla.kasnya@stud.uni-corvinus.hu>
2021-10-29 23:17:20 +0200

    6

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
@@ -22,6 +23,7 @@ namespace Webszolgaltatas
             GetExchangeRates();
             dataGridView1.DataSource = Rates;
             adatfeldolgozas();
+            Chartolas();
         }
         void GetExchangeRates()
         {
@@ -55,6 +57,24 @@ namespace Webszolgaltatas
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+        void Chartolas()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
