9d3868b10f4a6ba1ab8f8dde6d267d720ad245a8
Csilla Kasnya <csilla.kasnya@stud.uni-corvinus.hu>
2021-11-26 11:23:38 +0100

    jelszo

--- UnitTest/UnitTest/AccountControllerTestFixture.cs
+++ UnitTest/UnitTest/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UnitTest
@@ -26,6 +27,16 @@ namespace UnitTest
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            var kisbetu = new Regex(@"[a-z]+");
+            var nagybetu = new Regex(@"[A-Z]+");
+            var szam = new Regex(@"[0-9]+");
+            var eleghosszu = new Regex(@".{8,}");
+            return kisbetu.IsMatch(password) && nagybetu.IsMatch(password) && szam.IsMatch(password) && eleghosszu.IsMatch(password);
+
         }
     }
 }
