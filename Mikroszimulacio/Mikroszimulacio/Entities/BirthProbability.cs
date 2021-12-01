b0b04c69b2fab5e78f2366d76792cfa67bc2009e
Csilla Kasnya <csilla.kasnya@stud.uni-corvinus.hu>
2021-12-01 16:27:47 +0100

    ent

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int Gyermekszam { get; set; }
+        public double Szulval { get; set; }
+    }
+}
---
+++ Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    class DeathProbability
+    {
+        public Gender Nem { get; set; }
+        public int Eves { get; set; }
+        public double Halval { get; set; }
+    }
+}
